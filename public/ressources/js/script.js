7b4b6573cace65037e1a1ebf668df41543b36cdf
Vincent <vincent.durufle@hotmail.fr>
2019-04-03 18:26:13 +0200

    font fix 3

--- public/ressources/js/script.js
+++ public/ressources/js/script.js
@@ -15,7 +15,7 @@
 
 }
 (function () {
-    if (document.body.className === 'indexPage' && document.URL != ('http://sachalebas.com/photo' || 'https://sachalebas.com/photo')) {
+    if (document.body.className === 'indexPage' && document.URL !== ('https://sachalebas.com/photo')) {
         document.querySelector('[data-position="1"]').classList.add('is_selected');
         let rightCounter = document.querySelector('.is_selected');
         const leftCounter = document.querySelectorAll('[data-position]').length;
